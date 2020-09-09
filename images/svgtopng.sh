2c5cc823aabbeb28ce3963222868ee334b68d1ec
LaserTron <ntouikan@unb.ca>
2020-09-09 15:44:40 -0300

    Lecture 6

--- images/svgtopng.sh
+++ images/svgtopng.sh
@@ -2,4 +2,4 @@
 ##
 # A silly script to convert svg files decent looking png files
 ##
-for i in *.svg; do convert -density 100 universal_prop.svg universal_prop.png; done
+for i in *.svg; do convert -density 100 $i `basename $i .svg`.png; done
